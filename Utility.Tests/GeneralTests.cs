40872f66da1a3b207a04a03bf45a8cac10a46eea
Kippy <not@telling.ever>
2022-10-20 00:25:58 +0200

    add namespace ; remove dead code

--- Utility.Tests/GeneralTests.cs
+++ Utility.Tests/GeneralTests.cs
@@ -7,64 +7,62 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-public class GeneralTests
+namespace MidiMetronome.Tests
 {
-    readonly string[] MidiTestFiles = new string[]
+    public class GeneralTests
     {
-        "MIDI_sample",
-        "darude-sandstorm",
-        "dr.dre-still",
-        "Under-The-Sea-(From-'The-Little-Mermaid')"
-    };
-
-    [Test]
-    public void Dev()
-    {
-        var assets = AssetDatabase.FindAssets(nameof(GeneralTests))
-                                  .Select(guid => AssetDatabase.GUIDToAssetPath(guid))
-                                  .Select(path => AssetDatabase.LoadAssetAtPath<MonoScript>(path))
-                                  .Distinct();
-
-        var script = assets.FirstOrDefault();
-
-        if (script == null)
-            throw new Exception($"Can't locate the script in the project!");
-
-        var path = AssetDatabase.GetAssetPath(script);
-        var root = Path.GetDirectoryName(path);
-        var sampleDir = $@"{root}\Samples";
-
-        var midiFiles = Directory.GetFiles(sampleDir, "*.mid")
-                                 .Select(x => new { name = Path.GetFileName(x), rawMidi = File.ReadAllBytes(x) });
-
-        foreach (var x in midiFiles)
-            TestMidi(x.rawMidi, x.name);
-    }
-    void TestMidi(byte[] rawMidi, string name)
-    {
-
-        TickInfo[] ticks = null;
-
-        try
+        [Test]
+        public void SampleData()
         {
-            ticks = MetronomeUtility.GenerateBeats(rawMidi);
+            //Find script location
+            var assets = AssetDatabase.FindAssets(nameof(GeneralTests))
+                                      .Select(guid => AssetDatabase.GUIDToAssetPath(guid))
+                                      .Select(path => AssetDatabase.LoadAssetAtPath<MonoScript>(path))
+                                      .Distinct();
+
+            var script = assets.FirstOrDefault();
+
+            if (script == null)
+                throw new Exception($"Can't locate the script in the project!");
+
+            //Assamble samples folder path
+            var path = AssetDatabase.GetAssetPath(script);
+            var root = Path.GetDirectoryName(path);
+            var sampleDir = $@"{root}\Samples";
+
+            //Filter and load midis to memory
+            var midiFiles = Directory.GetFiles(sampleDir, "*.mid")
+                                     .Select(x => new { name = Path.GetFileName(x), rawMidi = File.ReadAllBytes(x) });
+
+            //TestMidis for integrity
+            foreach (var x in midiFiles)
+                TestMidi(x.rawMidi, x.name);
         }
-        catch (Exception e)
+        void TestMidi(byte[] rawMidi, string name)
         {
-            Debug.LogError(name);
-            Debug.LogException(e);
+            TickInfo[] ticks = null;
+
+            try
+            {
+                ticks = MetronomeUtility.GenerateBeats(rawMidi);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(name);
+                Debug.LogException(e);
+            }
+
+            if (ticks == null)
+                return;
+
+            Debug.Log($"{name}");
+
+            foreach (var x in ticks)
+            {
+                Debug.Log($"[{x.BPM:F0}] {x.Time:0.###}s");
+            }
+
+            Debug.Log("======================");
         }
-
-        if (ticks == null)
-            return;
-
-        Debug.Log($"{name}");
-
-        foreach (var x in ticks)
-        {
-            Debug.Log($"[{x.BPM:F0}] {x.Time:0.###}s");
-        }
-
-        Debug.Log("======================");
     }
 }
